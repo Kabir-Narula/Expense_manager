9cc83c554f6f3a57c6e595d3cd56dd483d8e4a96
seanMuniz <sean.muniz2000@gmail.com>
2025-03-01 13:42:37 -0500

    initial commit for sidebar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,14 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
+
+
+import SideNavbar from "../components/SideNavbar";
+
+
 
 function App() {
   return (
     <>
-      <h1 className="flex justify-center bg-amber-900">Hello</h1>
+      <SideNavbar/>
     </>
   );
 }
