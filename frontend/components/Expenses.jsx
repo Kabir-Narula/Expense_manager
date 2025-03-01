0119a1c48790c0f309bbf7fc8405d3ce78437c99
Sukhman Hara <shara1@myseneca.ca>
2025-03-01 15:42:53 -0500

    added navigation between components

---
+++ frontend/components/Expenses.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Expenses() {
+  return <div>Expenses Page (Coming soon...)</div>;
+}
+
+export default Expenses;
---
+++ frontend/components/Income.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Income() {
+  return <div>Income Page (Coming soon...)</div>;
+}
+
+export default Income;
