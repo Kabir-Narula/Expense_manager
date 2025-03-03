2e2bb16e858e39f5d6877a9d2438a3313a170a75
Kabir <AckerLevi102@gmail.com>
2025-03-03 14:51:37 -0500

    Updated frontend components with rechart and sample data in pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,26 +1,25 @@
-// import { React } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
-
 
 import SideNavbar from "../components/SideNavbar";
 import Dashboard from "../pages/Dashboard";
 import Expenses from "../pages/Expenses";
 import Income from "../pages/Income";
-import Logout from "../pages/Logout"; 
-
+import Logout from "../pages/Logout";
+import Charts from "../pages/Charts"; // Import Charts Page
 
 function App() {
   return (
-      <Router >
-        <SideNavbar/>
-          <Routes>
-            <Route path="/" element={<Dashboard />} />
-            <Route path="/expenses" element={<Expenses />} />
-            <Route path="/income" element={<Income />} />
-            <Route path="/logout" element={<Logout />} />
-          </Routes>
-      </Router>
+    <Router>
+      <SideNavbar />
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/expenses" element={<Expenses />} />
+        <Route path="/income" element={<Income />} />
+        <Route path="/logout" element={<Logout />} />
+        <Route path="/charts" element={<Charts />} /> {/* Added Charts Route */}
+      </Routes>
+    </Router>
   );
 }
 
