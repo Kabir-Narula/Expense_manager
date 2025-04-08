4b1f400a42854dd8c738b99ee73f1dc5eef36390
seanMuniz <sean.muniz2000@gmail.com>
2025-04-08 14:43:44 -0400

    added 200 status code on successful request

--- backend/controllers/expenseController.js
+++ backend/controllers/expenseController.js
@@ -4,9 +4,8 @@ export const getAllExpense = async (req, res) => {
     const userId = req.user.id
     try {
         console.log(userId)
-        console.log("e")
         const expense = await Expense.find({userId}).sort({date: -1});
-        res.json(expense);
+        res.status(200).json(expense);
 
     } catch (err) {
         res.status(500).json({message: "Server Error"});
@@ -17,10 +16,8 @@ export const updateExpense = async (req, res) => {
     const id = req.user.id
     try{
         const result = await Expense.updateOne({userId: id}, {$set: req.body}, {upsert: true});
-        res.json(result);
+        res.status(200).json(result);
     } catch (err){
         res.status(500).json({message: "Server Error"})
     }
-
-
 }
