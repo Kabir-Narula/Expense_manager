0e10b1028a0b903e1ff4a67c681751c2e37928d6
seanMuniz <sean.muniz2000@gmail.com>
2025-10-13 18:58:03 -0400

    fixed recurring logic

--- frontend/components/EditSource.jsx
+++ frontend/components/EditSource.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import api from "../src/Utils/api";
 
 export default function EditSource ({open, closeModal, type, incomeData }) {
