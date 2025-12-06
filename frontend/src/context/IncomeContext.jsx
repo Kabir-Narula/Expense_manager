d357674b275ee4cf9759007110385bb19fe248da
Sean Muniz <108829819+seanMuniz@users.noreply.github.com>
2025-12-05 21:59:55 -0500

    Remove logging for cached income data
    Removed console logging for cached income data.

--- frontend/src/context/IncomeContext.jsx
+++ frontend/src/context/IncomeContext.jsx
@@ -117,10 +117,6 @@ export const IncomeProvider = ({ children }) => {
         cacheKey += `-${INCOME_KEY}`
         const cachedData = incomeCache.current.get(cacheKey);
         if (cachedData) {
-          console.log(
-            "INCOME cached data: " + JSON.stringify(cachedData, null, 2),
-          );
-          console.log("INCOME Cache key: " + cacheKey);
           setIncomeUI(cachedData);
         } else {
           if (transactionOption === "Upcoming Transactions") {
